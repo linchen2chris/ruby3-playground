774e3e81fd7b70cb1745dd34fedb915c051f7894
Chen Lin <chen.lin@rea-group.com>
2021-04-06 22:00:16 +0800

    init create ruby 3 playground

---
+++ lib/ruby3/playground.rb
@@ -0,0 +1,8 @@
+require "ruby3/playground/version"
+
+module Ruby3
+  module Playground
+    class Error < StandardError; end
+    putc 'h'
+  end
+end
---
+++ lib/ruby3/playground/version.rb
@@ -0,0 +1,5 @@
+module Ruby3
+  module Playground
+    VERSION = "0.1.0"
+  end
+end

fe93e1f3d564c31307af8c2b282df34358c0ecda
Chen Lin <chen.lin@rea-group.com>
2021-04-08 18:04:08 +0800

    master 完成模板方法的学习

--- lib/ruby3/template/Report.rb
+++ lib/ruby3/template/Report.rb
@@ -7,7 +7,63 @@ module Ruby3
       end
 
       def output_report
-        "<html> <head> <title>#{@title}</title> <body> #{@text.map { |line| "<p>#{line}</p> " }.join}</body> </head> </html>"
+        start = output_start
+        head = output_head
+        body =
+          body_start + "#{@text.map { |line| output_line(line) }.join}" +
+            body_end
+        endText = output_end
+        start + head + body + endText
+      end
+
+      def output_start
+        ''
+      end
+      def output_head
+        ''
+      end
+      def output_line(line)
+        line
+      end
+      def body_start
+        ''
+      end
+      def body_end
+        ''
+      end
+      def output_end
+        ''
+      end
+    end
+    class HTMLReport < Report
+      def output_start
+        '<html><head></head>'
+      end
+      def output_head
+        "<title>#{@title}</title>"
+      end
+      def body_start
+        '<body>'
+      end
+      def body_end
+        '</body>'
+      end
+      def output_line(line)
+        "<p>#{line}</p>"
+      end
+      def output_end
+        '</html>'
+      end
+    end
+    class MarkDownReport < Report
+      def output_head
+        "##{@title}"
+      end
+      def body_start
+        "\n"
+      end
+      def output_line(line)
+        "#{line}\n"
       end
     end
   end
--- spec/ruby3/template/Report_spec.rb
+++ spec/ruby3/template/Report_spec.rb
@@ -1,9 +1,15 @@
 require 'ruby3/template/Report'
 RSpec.describe Ruby3::Template do
-  it 'display correct' do
-    report = Ruby3::Template::Report.new
+  it 'display html correct' do
+    report = Ruby3::Template::HTMLReport.new
     expect(report.output_report).to eq(
-      '<html> <head> <title>monthly report</title> <body> <p>Things are going</p> <p>really well</p> </body> </head> </html>'
+      '<html><head></head><title>monthly report</title><body><p>Things are going</p><p>really well</p></body></html>'
+    )
+  end
+  it 'display markdown correct' do
+    mdReport = Ruby3::Template::MarkDownReport.new
+    expect(mdReport.output_report).to eq(
+      "#monthly report\nThings are going\nreally well\n"
     )
   end
 end

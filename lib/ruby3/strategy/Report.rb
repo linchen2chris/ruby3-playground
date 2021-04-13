4a46fe056acb8cfb71f35c0d35ca639fe90046dd
Chen Lin <lc1990linux@gmail.com>
2021-04-13 17:19:40 +0800

    master use proc rewrite strategy

--- lib/ruby3/strategy/Report.rb
+++ lib/ruby3/strategy/Report.rb
@@ -2,13 +2,22 @@ class Report
   attr_reader :title, :text
   attr_accessor :formatter
 
-  def initialize(formatter)
+  def initialize(&formatter)
     @title = 'monthly report'
     @text = ['Things are going', 'really well']
     @formatter = formatter
   end
 
   def output_report
-    @formatter.output(self)
+    @formatter.call(self)
   end
 end
+
+HTML_FORMATTER =
+  lambda do |context|
+    "<html><head></head><title>#{context.title}</title><body>#{context.text.map { |line| "<p>#{line}</p>" }.join}</body></html>"
+  end
+MD_FORMATTER =
+  lambda do |context|
+    "##{context.title}\n#{context.text.map { |line| "#{line}\n" }.join}"
+  end
--- spec/ruby3/strategy/Report_spec.rb
+++ spec/ruby3/strategy/Report_spec.rb
@@ -1,25 +1,31 @@
 require 'ruby3/strategy/Report'
-require 'ruby3/strategy/HTMLFormatter'
-require 'ruby3/strategy/MDFormatter'
 
 RSpec.describe do
   it 'display html correct' do
-    htmlFormatter = HTMLFormatter.new
-    report = Report.new(htmlFormatter)
+    report = Report.new &HTML_FORMATTER
     expect(report.output_report).to eq(
       '<html><head></head><title>monthly report</title><body><p>Things are going</p><p>really well</p></body></html>'
     )
 
     #  change strategy
-    report.formatter = MDFormatter.new
+    report.formatter = MD_FORMATTER
     expect(report.output_report).to eq(
       "#monthly report\nThings are going\nreally well\n"
     )
   end
   it 'display markdown correct' do
-    mdReport = Report.new(MDFormatter.new)
+    mdReport = Report.new &MD_FORMATTER
     expect(mdReport.output_report).to eq(
       "#monthly report\nThings are going\nreally well\n"
+    )
+  end
+  it 'display plaint correctly' do
+    report =
+      Report.new do |context|
+        "#{context.title}: #{context.text.map { |x| x }.join}"
+      end
+    expect(report.output_report).to eq(
+      'monthly report: Things are goingreally well'
     )
   end
 end

ccc0674f431f7f8fc40d5feaaa5cddee0f75b1f2
Chen Lin <lc1990linux@gmail.com>
2021-05-11 17:08:45 +0800

    repl: and express

--- lib/ruby3/repl/Express.rb
+++ lib/ruby3/repl/Express.rb
@@ -27,3 +27,16 @@ class Bigger < Express
     results
   end
 end
+
+class And < Express
+  def initialize(exp1, exp2)
+    @exp1 = exp1
+    @exp2 = exp2
+  end
+
+  def evaluate(dir)
+    result1 = @exp1.evaluate(dir)
+    result2 = @exp2.evaluate(dir)
+    (result1 & result2)
+  end
+end
--- spec/ruby3/repl/Express_spec.rb
+++ spec/ruby3/repl/Express_spec.rb
@@ -3,18 +3,23 @@ RSpec.describe do
   it 'should find all' do
     all_expr = All.new
     files = all_expr.evaluate('./spec/ruby3/repl')
-    expect(files).to eql ['./spec/ruby3/repl/Express_spec.rb']
-  end
-
-  it 'should find big one' do
-    bigger = Bigger.new(200)
-    files = bigger.evaluate('./spec/ruby3/repl')
-    expect(files).to eql ['./spec/ruby3/repl/Express_spec.rb']
+    expect(files).to eql %w[
+          ./spec/ruby3/repl/2KB.txt
+          ./spec/ruby3/repl/Express_spec.rb
+        ]
   end
 
   it 'no big file' do
     bigger = Bigger.new(2000)
     files = bigger.evaluate('./spec/ruby3/repl')
-    expect(files).to eql []
+    expect(files).to eql ['./spec/ruby3/repl/2KB.txt']
+  end
+
+  it 'And works' do
+    all = All.new
+    bigger = Bigger.new(2000)
+    and_expr = And.new(all, bigger)
+    files = and_expr.evaluate('./spec/ruby3/repl')
+    expect(files).to eql(['./spec/ruby3/repl/2KB.txt'])
   end
 end
